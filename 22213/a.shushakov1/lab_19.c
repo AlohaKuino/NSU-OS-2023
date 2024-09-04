e0c6736a1c6c6793190e25cd9494db794df9da25
Александр Шушаков <127180614+AlohaKuino@users.noreply.github.com>
2024-09-04 12:50:20 +0700

    Create lab_19.c

---
+++ 22213/a.shushakov1/lab_19.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <fnmatch.h>
+#include <errno.h>
+
+#define MAX_PATTERN_LEN 256
+
+void check_error(void *ptr, const char *message) {
+    if (!ptr) {
+        perror(message);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void sanitize_input(char *input) {
+    if (strchr(input, '/')) {
+        fprintf(stderr, "Error: '/' is not allowed in the pattern.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main() {
+    char pattern[MAX_PATTERN_LEN];
+
+    printf("Enter the file name pattern: ");
+    if (!fgets(pattern, sizeof(pattern), stdin)) {
+        perror("Error reading input");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t len = strlen(pattern);
+    if (pattern[len - 1] == '\n') {
+        pattern[len - 1] = '\0';
+    }
+
+    sanitize_input(pattern);
+
+    DIR *directory = opendir(".");
+    check_error(directory, "Error opening directory");
+
+    struct dirent64 *entry;
+    int match_found = 0;
+
+    while (1) {
+        errno = 0;
+        entry = readdir64(directory);
+
+        if (entry == NULL) {
+            if (errno != 0) {
+                perror("Error reading directory");
+                if (closedir(directory) == -1) {
+                    perror("Error closing directory");
+                }
+                exit(EXIT_FAILURE);
+            }
+            break;
+        }
+
+        if (fnmatch(pattern, entry->d_name, 0) == 0) {
+            printf("%s\n", entry->d_name);
+            match_found = 1;
+        }
+    }
+
+    if (!match_found) {
+        fprintf(stderr, "No files matched the pattern: %s\n", pattern);
+    }
+
+    if (closedir(directory) == -1) {
+        perror("Error closing directory");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
